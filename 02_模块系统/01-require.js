d2c50b2ae0db8f490a733ef578a0926587388774
FlyThroughMountains <zj18035189565@163.com>
2019-05-05 11:25:29 +0800

    第一次使用git,提交Nodejs的学习笔记,并同步到GitHub上。

---
+++ 02_模块系统/01-require.js
@@ -0,0 +1,11 @@
+/**
+ * @Description: 导入文件 (引入自己写的摸一个文件，必须从./开始，即使是同一个目录下的文件)
+ * @author mountain
+ * @date 2019/5/4
+ * @time 23:04
+ * @log
+ */
+// TODO 将要做环没有做的任务
+// FIXME 需要修复的问题
+let result = require("./02.js");// js中的function的obj类型
+console.log(result);
---
+++ 02_模块系统/02.js
@@ -0,0 +1,5 @@
+// console.log("Hello World");
+let a = "mountain";
+// global.a = a;
+
+exports.name = a;
